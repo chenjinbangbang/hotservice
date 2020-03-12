d805963b8ed6991cc2b04f9f1f08045fd13bef2d
chenjinbangbang <905690338@qq.com>
2020-03-13 00:25:58 +0800

    user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Body, Put, ForbiddenException, Res } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+import { UserDto } from './dto/user.dto';
+import { ApiBody, ApiTags, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('user') // 要将控制器附加到特定标签
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Get('list')
+  @ApiResponse({ type: [UserDto] }) // 响应的模型
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Put()
+  // @ApiBody({ type: [UserDto] }) // 
+  async update(@Body() body: UserDto) {
+    console.log(body)
+    await this.userService.update(body);
+
+  }
+
+}
